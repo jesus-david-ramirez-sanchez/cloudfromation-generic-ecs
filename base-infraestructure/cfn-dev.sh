42014b0f89b39565b9bcdeff8e5512a3cdea0db6
Jesus David Ramirez <jesus.remirez@clicoh.com>
2024-12-19 16:15:23 -0500

    Agregar soporte para el dominio en la configuración de CloudFormation y actualizar puertos de Load Balancer a 443

--- base-infraestructure/cfn-dev.sh
+++ base-infraestructure/cfn-dev.sh
@@ -7,5 +7,6 @@ aws cloudformation deploy --template-file packaged.yaml --stack-name most-wanted
     minSize=1 \
     maxSize=5 \
     InstanceType=t3a.nano \
+    Domain=api.jedramir.com \
     SSLCertificate=arn:aws:acm:us-east-1:712779586037:certificate/0915e42c-12d5-47e1-9b17-0351c9d51e89 \
 # aws cloudformation update-termination-protection --stack-name most-wanted-dev --enable-termination-protection --profile personal
